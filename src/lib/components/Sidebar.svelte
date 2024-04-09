dee29dad52f9fd1ac9c45b00f3174693721994ac
chejel <chejenj@gmail.com>
2024-04-09 17:50:18 -0400

    Add sidebar

---
+++ src/lib/components/Sidebar.svelte
@@ -0,0 +1,30 @@
+<script>
+	// Import icon components
+	import CloseIcon from '$lib/components/icons/CloseCircle.svelte';
+
+	// Set state of sidebar
+	export let sidebarVisible;
+</script>
+
+<slot />
+
+<!-- Close button -->
+<button
+	class="close-button"
+	aria-label="Close"
+	on:click|stopPropagation={() => {
+		sidebarVisible = false;
+	}}><CloseIcon /></button
+>
+
+<style>
+	.close-button {
+		position: absolute;
+		top: 6px;
+		right: 0px;
+		background-color: transparent;
+		border: 0;
+		cursor: pointer;
+		z-index: 100;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,61 @@
 <script>
 	// Import components
 	import Map from '$lib/components/Map.svelte';
+	import Sidebar from '$lib/components/Sidebar.svelte';
+
+	// Import icon components
+	import ListIcon from '$lib/components/icons/List.svelte';
+
+	// Import transition
+	import { fade } from 'svelte/transition';
+
+	// Set state of sidebar
+	let sidebarVisible = true;
 </script>
 
-<Map />
+<Map bind:sidebarVisible />
+
+{#if sidebarVisible}
+	<div class="sidebar-content" transition:fade={{ duration: 300 }}>
+		<Sidebar bind:sidebarVisible>
+			<header>
+				<h1>New York State Media Outlets</h1>
+			</header>
+		</Sidebar>
+	</div>
+{:else}
+	<button
+		aria-label="Show sidebar"
+		class="sidebar-collapsed"
+		on:click|stopPropagation={() => (sidebarVisible = !sidebarVisible)}
+	>
+		<ListIcon />
+	</button>
+{/if}
+
+<style>
+	.sidebar-content {
+		position: relative;
+		max-width: 375px;
+		max-height: calc(100svh - 4rem);
+		border-radius: 5px;
+		background-color: rgba(23, 23, 23, 0.75);
+		top: 0;
+		z-index: 1;
+		margin: 1rem;
+		box-shadow: 0px 0px 24px 3px rgba(255, 255, 255, 0.2);
+		display: flex;
+		flex-direction: column;
+		overflow: hidden;
+		z-index: 2;
+	}
+
+	.sidebar-collapsed {
+		position: absolute;
+		top: 0;
+		margin: 1rem;
+		cursor: pointer;
+		background-color: transparent;
+		border: 0;
+	}
+</style>
