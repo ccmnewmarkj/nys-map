95e1477e1a3ac554627b0ad7a452f9b380fd9245
chejel <chejenj@gmail.com>
2024-04-12 12:29:59 -0400

    Add panels

---
+++ src/lib/components/panels/About.svelte
@@ -0,0 +1,32 @@
+<script>
+	export let outletCount;
+</script>
+
+<p>
+	This map of <span style="font-weight: 700;"
+		>community media outlets in New York State, north of New York City</span
+	>, includes {outletCount}
+	outlets that primarily cover communities of color and immigrants. The map accompanies the New York
+	State Media Directory, which together serve as a resource for advertisers, journalists, community organizers,
+	and others who want to connect with the community media sector in New York State. You can see the list
+	of outlets under the <span class="tab">Outlets</span> button
+</p>
+<p class="callout" style="margin-top: 1rem;">
+	For community media outlets in New York City, visit CCM's <a
+		href="http://ccm-directory.journalism.cuny.edu">Many Voices, One City</a
+	> directory.
+</p>
+
+<h2>How to use the map</h2>
+<p>
+	Select the <span class="tab">Search</span> button to apply filters that will narrow down the
+	number of outlets, whether by community, language or media type. The list of outlets that appear
+	under the
+	<span class="tab">Outlets</span> button will update accordingly.
+</p>
+
+<h2>Reach out</h2>
+<p>
+	If you have any feedback, corrections or outlets that fit the criteria of the directory, please
+	email us at <a href="mailto:ccm@journalism.cuny.edu">ccm@journalism.cuny.edu</a>.
+</p>
---
+++ src/lib/components/panels/Outlets.svelte
@@ -0,0 +1,9 @@
+<script>
+	// Import stores
+	import { directoryData, filteredDirectory } from '$lib/stores.js';
+</script>
+
+<p><strong>List of outlets</strong></p>
+{#each $filteredDirectory?.features as outlet}
+	<p>{outlet.properties['Media Outlet']}</p>
+{/each}
