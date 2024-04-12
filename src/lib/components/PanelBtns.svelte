55e232f8924c39b201139c8079b48f1f0c148649
chejel <chejenj@gmail.com>
2024-04-12 12:28:43 -0400

    Add panel tabs

--- src/lib/components/PanelBtns.svelte
+++ src/lib/components/PanelBtns.svelte
@@ -5,6 +5,8 @@
 
 	// Import stores
 	import { aboutPanelVisible, searchPanelVisible, outletPanelVisible } from '$lib/stores.js';
+
+	export let badgeCount = '';
 </script>
 
 <div class="btn-container">
@@ -30,19 +32,22 @@
 			$aboutPanelVisible = false;
 			$searchPanelVisible = false;
 			$outletPanelVisible = true;
-		}}>Outlets</button
+		}}
+		>Outlets {#if badgeCount}
+			<span class="badge-count">{badgeCount}</span>
+		{/if}</button
 	>
 </div>
 
 <style>
 	.btn-container {
 		display: flex;
-		border-top: 0.75px solid var(--orange);
-		border-bottom: 3px solid #eee;
+		border-top: 1px solid rgba(248, 181, 0, 0.5);
+		border-bottom: 2px solid rgba(248, 181, 0, 0.45);
 	}
 
 	button {
-		background-color: #fffffd;
+		background-color: var(--light-gray);
 		font-size: 1rem;
 		font-weight: 600;
 		text-transform: uppercase;
@@ -55,12 +60,29 @@
 	}
 
 	button:not(:last-child) {
-		border-right: 1px solid #eee;
+		border-right: 1px solid #fffffd;
 	}
 
 	.active {
-		box-shadow: inset 0px -3px 0px 0px var(--green);
+		/* box-shadow: inset 0px -1px 0px 0px rgba(248, 181, 0, 1); */
+		background-color: var(--white);
 		font-weight: 700;
 		cursor: auto;
+	}
+
+	.badge-count {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		padding: 0.1rem 0.4rem;
+		margin-left: 0.2rem;
+		font-family: 'Open Sans', sans-serif;
+		font-size: 0.7rem;
+		font-weight: 700;
+		line-height: 1.4;
+		color: var(--white);
+		text-align: center;
+		background-color: var(--cerulean);
+		border-radius: 10px;
 	}
 </style>
--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -10,35 +10,51 @@
 	import CloseIcon from '$lib/components/icons/CloseCircle.svelte';
 
 	// Import stores
-	import { aboutPanelVisible, searchPanelVisible, outletPanelVisible } from '$lib/stores.js';
+	import {
+		directoryData,
+		filteredDirectory,
+		aboutPanelVisible,
+		searchPanelVisible,
+		outletPanelVisible
+	} from '$lib/stores.js';
 
 	// Import transition
 	import { fade } from 'svelte/transition';
 
 	// Set state of sidebar
 	export let sidebarVisible;
+
+	// Outlet count
+	$: outletCount = $filteredDirectory.features?.length;
 </script>
 
 <header>
 	<h1>NYS Media Map</h1>
-	<PanelBtns />
+	<p>
+		A project of the <a
+			href="https://www.journalism.cuny.edu/centers/center-community-media/advertising-boost-initiative/"
+			>Advertising Boost Initiative</a
+		> at the Center for Community Media
+	</p>
 </header>
 
-<main>
-	{#if $aboutPanelVisible}
-		<div class="panel-body" in:fade>
-			<AboutPanel />
-		</div>
-	{:else if $searchPanelVisible}
-		<div class="panel-body" in:fade>
-			<SearchPanel />
-		</div>
-	{:else if $outletPanelVisible}
-		<div class="panel-body" in:fade>
-			<OutletPanel />
-		</div>
-	{/if}
-</main>
+<nav>
+	<PanelBtns badgeCount={outletCount} />
+</nav>
+
+{#if $aboutPanelVisible}
+	<main class="panel-body" in:fade>
+		<AboutPanel {outletCount} />
+	</main>
+{:else if $searchPanelVisible}
+	<main class="panel-body" in:fade>
+		<SearchPanel />
+	</main>
+{:else if $outletPanelVisible}
+	<main class="panel-body" in:fade>
+		<OutletPanel />
+	</main>
+{/if}
 
 <!-- Close button -->
 <button
@@ -55,7 +71,7 @@
 		max-height: calc(100svh - 4rem);
 		display: flex;
 		flex-direction: column;
-		overflow: hidden;
+		overflow: auto;
 	}
 
 	.close-button {
@@ -66,5 +82,28 @@
 		border: 0;
 		cursor: pointer;
 		z-index: 100;
+	}
+
+	header {
+		padding: 1rem 1rem 0.5rem 1rem;
+
+		/* Source: https://uigradients.com */
+		background: #f8b500; /* fallback for old browsers */
+		background: -webkit-linear-gradient(
+			to bottom,
+			#f8b500,
+			#f9f07a
+		); /* Chrome 10-25, Safari 5.1-6 */
+		background: linear-gradient(
+			to bottom,
+			#f8b500,
+			#f9f07a
+		); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+		background: linear-gradient(180deg, rgba(91, 188, 255, 0.75) 5%, rgb(255, 241, 188) 90%);
+
+		background: linear-gradient(180deg, rgba(55, 140, 231, 0.5) 10%, rgb(255, 241, 188) 90%);
+
+		background: linear-gradient(180deg, rgba(59, 188, 241, 0.5) 10%, rgb(255, 241, 188) 90%);
 	}
 </style>
