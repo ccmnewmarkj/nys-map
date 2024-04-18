30a03a1956fd77128c9433c00dad94ec59f930b4
chejel <chejenj@gmail.com>
2024-04-18 16:06:39 -0400

    Add map popup

--- src/lib/components/Map.svelte
+++ src/lib/components/Map.svelte
@@ -52,6 +52,13 @@
 	let initialCenterLng;
 	let movedCenterLng;
 
+	// Color by format
+	const newspaperFormat = '250, 112, 112';
+	const radioFormat = '64, 162, 227';
+	const magazineFormat = '115, 144, 114';
+	const digitalFormat = '255, 201, 74';
+	const otherFormat = '179, 200, 207';
+
 	onMount(() => {
 		// Set up map via store
 		map.set(
@@ -257,35 +264,235 @@
 				type: 'circle',
 				source: 'outlets',
 				paint: {
-					'circle-radius': 6,
-					'circle-stroke-width': 1.5,
-					'circle-color': 'rgba(255, 130, 0, 0.9)',
-					'circle-stroke-color': 'white'
+					'circle-radius': {
+						base: 1.75,
+						stops: [
+							[8, 6],
+							[15, 24]
+						]
+					},
+					'circle-stroke-width': 1.25,
+					'circle-stroke-color': 'white',
+					'circle-color': [
+						'match',
+						['get', 'Primary Format'],
+						'Newspaper',
+						`rgba(${newspaperFormat}, 1)`,
+						'Radio',
+						`rgba(${radioFormat}, 1)`,
+						'Magazine',
+						`rgba(${magazineFormat}, 1)`,
+						'Digital only',
+						`rgba(${digitalFormat}, 1)`,
+						/* other */ `rgba(${otherFormat}, 1)`
+					]
 				}
 			});
 
-			// cursor becomes pointer when on point
+			// Cursor becomes pointer when on point
 			$map.on('mouseenter', 'outlet-layer', () => {
 				$map.getCanvas().style.cursor = 'pointer';
 			});
 
-			// cursor goes back to default off point
+			// Cursor goes back to default off point
 			$map.on('mouseleave', 'outlet-layer', () => {
 				$map.getCanvas().style.cursor = '';
 			});
+
+			// Same for outlet search layer
+			$map.on('mouseenter', 'outlet-search-layer', () => {
+				$map.getCanvas().style.cursor = 'pointer';
+			});
+
+			$map.on('mouseleave', 'outlet-search-layer', () => {
+				$map.getCanvas().style.cursor = '';
+			});
+
+			///////////////////////////
+			// HIGHLIGHTED OUTLET from Outlet Search
+			///////////////////////////
+
+			$map.addLayer({
+				id: 'outlet-search-layer',
+				type: 'circle',
+				source: 'outlets',
+				paint: {
+					'circle-radius': {
+						base: 1.75,
+						stops: [
+							[8, 8],
+							[15, 30]
+						]
+					},
+					//'circle-radius': 8,
+					'circle-stroke-width': 2.5,
+					'circle-stroke-color': 'yellow',
+					'circle-color': [
+						'match',
+						['get', 'Primary Format'],
+						'Newspaper',
+						`rgba(${newspaperFormat}, 1)`,
+						'Radio',
+						`rgba(${radioFormat}, 1)`,
+						'Magazine',
+						`rgba(${magazineFormat}, 1)`,
+						'Digital only',
+						`rgba(${digitalFormat}, 1)`,
+						/* other */ `rgba(${otherFormat}, 1)`
+					]
+				}
+			});
+
+			// Initially hide all markers in outlet-search-layer
+			// When outlet name is selected, circle will be in different opacity
+			$map.setFilter('outlet-search-layer', ['in', 'Media Outlet', '']);
 
 			///////////////////////////
 			// POPUP
 			///////////////////////////
 
-			$map.on('click', ['outlet-layer'], (e) => {
+			$map.on('click', ['outlet-layer', 'outlet-search-layer'], (e) => {
 				const coordinates = e.features[0].geometry.coordinates.slice();
+
+				// "Ensure that if the map is zoomed out such that multiple
+				// copies of the feature are visible, the popup appears
+				// over the copy being pointed to."
+				while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+					coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+				}
+
 				//selectedOutlet.set(e.features[0].properties['Media Outlet']);
 
-				///// popup content
+				// Background of header section based on format
+				let headerBg;
+				if (e.features[0].properties['Primary Format'] === 'Newspaper') {
+					headerBg = newspaperFormat;
+				} else if (e.features[0].properties['Primary Format'] === 'Radio') {
+					headerBg = radioFormat;
+				} else if (e.features[0].properties['Primary Format'] === 'Magazine') {
+					headerBg = magazineFormat;
+				} else if (e.features[0].properties['Primary Format'] === 'Digital only') {
+					headerBg = digitalFormat;
+				} else {
+					headerBg = otherFormat;
+				}
+
+				// Icon source: https://iconoir.com/
+				const arrowUpRightIcon = `
+				<svg
+					width="20px"
+					height="20px"
+					viewBox="0 0 24 24"
+					stroke-width="1.75"
+					fill="none"
+					xmlns="http://www.w3.org/2000/svg"
+					color="#000000"
+					><path
+						d="M9.17137 14.8284L14.8282 9.17152M14.8282 9.17152H9.87848M14.8282 9.17152V14.1213"
+						stroke="rgba(${headerBg}, 1)"
+						stroke-width="1.75"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					></path><path
+						d="M21 3.6V20.4C21 20.7314 20.7314 21 20.4 21H3.6C3.26863 21 3 20.7314 3 20.4V3.6C3 3.26863 3.26863 3 3.6 3H20.4C20.7314 3 21 3.26863 21 3.6Z"
+						stroke="rgba(${headerBg}, 1)"
+						stroke-width="1.75"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					></path></svg
+				>
+				`;
+
+				const infoIcon = `
+				<svg
+					width="12px"
+					height="12px"
+					stroke-width="2.5"
+					viewBox="0 0 24 24"
+					fill="none"
+					xmlns="http://www.w3.org/2000/svg"
+					color="#000000"
+					><path
+						d="M12 11.5V16.5"
+						stroke="#000000"
+						stroke-width="2.5"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					></path><path
+						d="M12 7.51L12.01 7.49889"
+						stroke="#000000"
+						stroke-width="2.5"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					></path><path
+						d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
+						stroke="#000000"
+						stroke-width="2.1"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					></path></svg
+				>
+				`;
+
+				// Conditional popup values
+
+				let yearFoundedValue = e.features[0].properties['Year Founded']
+					? ` • Founded in ${e.features[0].properties['Year Founded']}`
+					: '';
+
+				let communityLabelValue =
+					JSON.parse(e.features[0].properties['Community']).length > 1
+						? 'Communities'
+						: 'Community';
+
+				let languageLabelValue =
+					JSON.parse(e.features[0].properties['Language']).length > 1 ? 'Languages' : 'Language';
+
+				let linkValue = e.features[0].properties['Website']
+					? `<a href="${e.features[0].properties['Website']}" target="_blank" class="popup-link-btn" style="color: rgba(${headerBg}, 1); border: 1px solid rgba(${headerBg}, 0.5);">Visit Website ${arrowUpRightIcon}</a>`
+					: '';
+
+				let locationNotes = e.features[0].properties['Location Status']
+					? `${infoIcon} Map location is approximate`
+					: '';
+
+				// POPUP
+
 				const popupContent = `
 				
-				${e.features[0].properties['Media Outlet']}
+			<div class="popup-header" style="background-color: rgba(${headerBg}, 0.5);">
+				<p class="popup-outlet-name">${e.features[0].properties['Media Outlet']}</p>
+				<p class="popup-location">${e.features[0].properties['City']}, NY ${yearFoundedValue}</p>
+			</div>
+
+			<div class="popup-body">
+				<div class="popup-row">
+					<p class="popup-category-label">Primary Format</p>
+					<p class="popup-category-value">${e.features[0].properties['Primary Format']}</p>
+				</div>
+
+				<hr />
+
+				<div class="popup-row">
+					<p class="popup-category-label">${communityLabelValue}</p>
+					<p class="popup-category-value">${JSON.parse(e.features[0].properties['Community']).join(', ')}</p>
+				</div>
+
+				<hr />
+
+				<div class="popup-row">
+					<p class="popup-category-label">${languageLabelValue}</p>
+					<p class="popup-category-value">${JSON.parse(e.features[0].properties['Language']).join(', ')}</p>
+				</div>
+
+				<hr />
+
+				${linkValue}
+			</div>
+
+			<div class="popup-footer" style="background-color: rgba(${headerBg}, 0.15);">
+				${locationNotes}
+			</div>
 				
 				`;
 
@@ -325,6 +532,14 @@
 	// Polygon toggles
 	export let showCounties = false;
 	export let showRegions = false;
+
+	// Show selected outlet circle on map
+	$: if ($selectedOutlet) {
+		$map.setFilter('outlet-search-layer', [
+			'any',
+			['in', $selectedOutlet, ['get', 'Media Outlet']]
+		]);
+	}
 </script>
 
 <!-- Map -->
